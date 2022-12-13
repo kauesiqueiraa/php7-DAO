3de0d5f0750b28c8e5caccc12117bb596759461b
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2022-12-13 18:20:43 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,22 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$123");
+
+echo $usuario;
 ?>
